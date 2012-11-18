4f4eaffd97fcd0c1fc992c277c693bfb89cdc152
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-11-18 20:54:06 +0000

    initial commit

---
+++ DisplayModesAndRenderingDemo/DisplayModesAndRenderingDemo/App_Start/DisplayMode.Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages;
+using System.Collections.Generic;
+
+
+namespace DisplayModesAndRenderingDemo
+{
+    public class DisplayModeConfig
+    {
+        public static void RegisterDisplayModes(IList<IDisplayMode> modes)
+        {       
+            modes.Insert(0, new DefaultDisplayMode("android")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
+                  ("Android", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+            modes.Insert(1, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
+                  ("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+        }
+    }
+}
---
+++ DisplayModesAndRenderingDemo/DisplayModesAndRenderingDemo/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace DisplayModesAndRenderingDemo
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+
+            // Demo:
+            // access /Home/Index from desktop browser, see Index.cshtml view
+            // access /Home/Index from iPhone, see Index.iphone.cshtml view
+            // access /Home/Index from Android, see Index.android.cshtml view 
+            // comment out iphone display mode
+            // access /Home/Index from iPhone, see Index.mobile.cshtml view 
+            // can use simulator or Firefox with User Agent Switcher extension
+            DisplayModeConfig.RegisterDisplayModes(DisplayModeProvider.Instance.Modes);
+        }
+    }
+}
